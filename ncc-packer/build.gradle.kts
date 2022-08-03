25c772cf2217d2a27ad756baa570b580d74ffba1
Nikolai Wotton <nwotton@cmtelematics.com>
2022-08-03 19:54:32 -0400

    bumping some dependencies

--- ncc-packer/build.gradle.kts
+++ ncc-packer/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+
 plugins {
     kotlin("js")
 }
@@ -24,11 +26,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-nodejs:0.0.7")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.208-kotlin-1.5.10")
-    implementation(npm("@vercel/ncc", "0.28.6", generateExternals = false))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.363")
+    implementation(npm("@vercel/ncc", "0.34.0", generateExternals = false))
 }
 
 val TaskContainer.compileKotlinJs
-    get() = named<org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile>("compileKotlinJs")
+    get() = named<Kotlin2JsCompile>("compileKotlinJs")
