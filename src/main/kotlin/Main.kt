69d889228c15c60662aafc2c592dddebe2744ca1
Nikolai Wotton <nwotton@cmtelematics.com>
2022-08-03 22:26:34 -0400

    testing self

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,58 @@
+import NodeJS.get
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.js.Js
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import models.Inputs
-import utils.actions.ActionsEnvironment
 import utils.actions.getInput
-import utils.actions.group
-import utils.failOrError
+import utils.actions.setFailed
 
-fun main() {
-    println("HIT THE FUCKING ENTRY POINT")
+suspend fun main() {
     val inputs = resolveInputs()
-    print("$inputs")
+    val numCommits: Int
     try {
-        println("Hello World From GitHub Actions!")
+        val client = HttpClient(Js) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(Json)
+            }
+        }
+        numCommits = client.get<JsonObject>(inputs.repoUrl) {
+            header("Accept", "application/vnd.github+json")
+            header("Authorization", "token ${inputs.token}")
+        }["commits"]!!.jsonPrimitive.toString().toInt()
+        client.close()
     } catch (e: Exception) {
-        failOrError(e.message ?: "Error while generating changelog")
+        setFailed("Max-Commit-Count failed to run due to exception")
+        throw e
+    }
+    if (numCommits > inputs.maxCommits) {
+        setFailed("Your PR contains $numCommits commits, but you are only allowed up to ${inputs.maxCommits} per PR")
     }
 }
 
-fun resolveInputs() = group("Reading input values") {
+fun resolveInputs(): Inputs {
     val inputString =
         getInput("maxCommits").ifEmpty { throw IllegalArgumentException("Don't forget to define maxCommits") }
     val inputNumber = inputString.toIntOrNull() ?: throw IllegalArgumentException("$inputString is not a valid number")
-    val token = getInput("token").ifEmpty { ActionsEnvironment.GITHUB_TOKEN }
-    return@group Inputs(
-        maxCommits = inputNumber,
-        token = token
+    val token =
+        getInput("token").ifEmpty { getInput("GITHUB_TOKEN").ifEmpty { throw IllegalArgumentException("Don't forget to set token") } }
+    val repo = getEnv("GITHUB_REPOSITORY")
+    val prNumber = getEnv("GITHUB_REF_NAME").split("/")[0].toIntOrNull()
+        ?: throw IllegalStateException("Failed to parse PR number")
+    require(getEnv("GITHUB_REF_TYPE") == "branch") { "This step should only be triggered by pull requests" }
+    require(getEnv("GITHUB_EVENT_NAME") == "pull_request") { "This step should only be triggerd by a pull request" }
+    val repoUrl = "https://api.github.com/repos/$repo/pulls/$prNumber"
+    return Inputs(maxCommits = inputNumber, token = token, repoUrl = repoUrl)
+}
+
+fun getEnv(name: String): String {
+    val errorString = "Somehow unable to find $name"
+    return process.env[name]?.ifEmpty { throw IllegalStateException(errorString) } ?: throw IllegalStateException(
+        errorString
     )
 }
--- src/main/kotlin/models/Inputs.kt
+++ src/main/kotlin/models/Inputs.kt
@@ -1,3 +1,3 @@
 package models
 
-data class Inputs(val maxCommits: Int, val token: String)
+data class Inputs(val maxCommits: Int, val token: String, val repoUrl: String)
