e9c64bf4c722c766a69703ef133b08f3ce8caf62
Nikolai Wotton <nwotton@cmtelematics.com>
2022-08-05 17:20:28 -0400

    attempting change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,8 +16,14 @@ fun main() {
 }
 
 fun resolveInputs() = group("Reading input values") {
+    println("Resolving Inputs")
+    val inputString = getInput("maxCommits")
+    println("Got input string: $inputString")
+    println("Number version: ${inputString.toInt()}")
+    val maxCommits = (getInput("maxCommits") ?: "").toInt()
+    val token = getInput("token").ifEmpty { ActionsEnvironment.GITHUB_TOKEN }
     return@group Inputs(
-        maxCommits = getInput("maxCommits").toInt(),
-        token = getInput("token").ifEmpty { ActionsEnvironment.GITHUB_TOKEN }
+        maxCommits = maxCommits,
+        token = token
     )
 }
